a3d11b0dbfab4cc4589edbbf2bd2eb05fd3a776b
Ville Lankinen <villelank@gmail.com>
2019-11-08 15:23:19 +0200

    maiden tiedot 2.12 lisätty

--- osa02/puhelinluettelo/src/App.js
+++ osa02/puhelinluettelo/src/App.js
@@ -1,18 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = props =>
   <p>{props.name} {props.number}</p>
 
+const Persons = ({persons, newFilter}) => (
+  persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
+    .map(person => <Person key={person.name} name={person.name} number={person.number}/>)
+)
+
+const Filter = ({filterValue, handleFilterChange}) => (
+  <div>
+    filter shown with <input value={filterValue} onChange={handleFilterChange}/>
+  </div>
+)
+
+const PersonForm = ({addName, newName, newNumber, handleNameChange, handleNumberChange}) => (
+  <form onSubmit={addName}>
+    <div>
+      name: <input value={newName} onChange={handleNameChange}/>
+    </div>
+    <div>
+      number: <input value={newNumber} onChange={handleNumberChange}/>
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
+
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '050-000000' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -45,33 +78,15 @@ const App = () => {
     }         
   }
   
-  const Rows = props => (
-    props.persons.filter(person => person.name.toLowerCase().includes(newFilter.toLowerCase()))
-      .map(person => <Person key={person.name} name={person.name} number={person.number}/>)
-  )
-  
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={newFilter} onChange={handleFilterChange}/>
-      </div>
-      <form onSubmit={addName}>
-        <div>
-          <h2>add a new</h2>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <Rows persons={persons} />
+      <Filter filterValue={newFilter} handleFilterChange={handleFilterChange}/>
+      <h3>add a new</h3>
+      <PersonForm addName={addName} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>      
+      <h3>Numbers</h3>
+      <Persons persons={persons} newFilter={newFilter} />
     </div>
-    
   )
 
 }
